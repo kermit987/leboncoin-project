a6ae8547f8b06f2406b31fe4fd296eef166a709f
steven <steven.loofat@yahoo.fr>
2021-06-17 12:37:12 +0200

    [+] modifying fizz-buzz.controler.js + adding another test to it

--- src/controller/fizz-buzz.controller.js
+++ src/controller/fizz-buzz.controller.js
@@ -1,11 +1,10 @@
-const express = require('express')
+// const express = require('express')
 
 const fizzBuzzController = async (req, res) => {
   const { int1, int2, limit, str1, str2} = req.body
 
   const result = []
-  // failed au niveau du type de int1 et int2
-  for (i = 1; i != limit; i++) {
+  for (i = 1; i <= limit; i++) {
     if (i % int1 === 0 && i % int2 === 0)
       result.push(str1 + str2)
     else if (i % int1 === 0) 
--- test/fizz-buzz.test.js
+++ test/fizz-buzz.test.js
@@ -10,7 +10,26 @@ describe('/POST testing fizz-buzz', () => {
       str1: "three",
       str2: "four"
     }
-    const expectResult = ['1','2','three','four','5','three','7','four','three','10','11','threefour','13','14','three','four','17','three','19']
+    const expectResult = ['1','2','three','four','5','three','7','four','three','10','11','threefour','13','14','three','four','17','three','19',"four"]
+    await request(app)
+      .post('/fizz-buzz')
+      .set('Accept', 'application/json')
+      .send(payload)
+      .expect(200)
+      .then(response => {
+        expect(JSON.parse(response.text)).toStrictEqual(expectResult)
+      })
+    done()
+  })
+  test('testing another normal behavior', async (done) => {
+    const payload = {
+      int1: 5,
+      int2: 6,
+      limit: 30,
+      str1: "five",
+      str2: "six"
+    }
+    const expectResult = ['1','2','3','4','five','six','7','8','9','five','11','six','13','14','five','16','17','six','19','five','21','22','23','six','five','26','27','28','29','fivesix']
     await request(app)
       .post('/fizz-buzz')
       .set('Accept', 'application/json')
