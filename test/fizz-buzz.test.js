b6f385988c95eca431fdbd27f2e7ab3190c6c7e5
steven <steven.loofat@yahoo.fr>
2021-06-20 13:24:25 +0200

    [*] clean code in fizz-buzz.test.js

--- test/fizz-buzz.test.js
+++ test/fizz-buzz.test.js
@@ -153,16 +153,6 @@ describe('/GET test getStatic ', () => {
   })
 })
 
-// describe('arrayContaining', () => {
-//   const expected = ['Alice', 'Bob'];
-//   it('matches even if received contains additional elements', () => {
-//     expect(['Alice', 'Bob', 'Eve']).toEqual(expect.arrayContaining(expected));
-//   });
-//   it('does not match if received does not contain expected elements', () => {
-//     expect(['Bob', 'Eve']).not.toEqual(expect.arrayContaining(expected));
-//   });
-// });
-
 afterAll(() => {
   server.close()
   client.close()
