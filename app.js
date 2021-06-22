f578b2ad7ea11ce0671c6868c5cf7172d80dbc73
steven <steven.loofat@yahoo.fr>
2021-06-22 15:40:00 +0200

    [+] Allow CORS

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ const dotenv = require('dotenv').config()
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true) //Otherwise there is an error while push on HEROKU
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization' //https://stackoverflow.com/questions/44245588/how-to-send-authorization-header-with-axios
+  )
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+  )
+  next()
+})
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
